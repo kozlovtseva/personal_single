c89ab361c8d90528db56614249b5677f00404a28
Alina <ka-front@fulogy.com>
2020-09-18 11:05:22 +0300

    add theme and media queries

---
+++ src/Theme.tsx
@@ -0,0 +1,25 @@
+import baseStyled, { ThemedStyledInterface } from "styled-components";
+
+const customMediaQuery = (maxWidth: number) =>
+    `@media (max-width: ${maxWidth}px)`;
+
+export const media = {
+    xs: customMediaQuery(480),
+    sm: customMediaQuery(576),
+    md: customMediaQuery(768),
+    lg: customMediaQuery(992),
+    xl: customMediaQuery(1200),
+    xxl: customMediaQuery(1600),
+};
+
+export const theme = {
+    color: {
+        primary: "#9BE9E9",
+        lightText: "#fff",
+        lightDarkText: "#777",
+        darkText: "#000",
+    },
+};
+
+export type Theme = typeof theme;
+export const styled = baseStyled as ThemedStyledInterface<Theme>;
